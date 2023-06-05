320598892b0f942541a3a6c663a2c309d5e1e3ab
Vihimuro <vihi1208@gmail.com>
2023-06-04 22:34:25 -0300

    CRUD Scaffolding

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -5,10 +5,12 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMVC.Models
 
 namespace SalesWebMVC
 {
@@ -33,6 +35,9 @@ namespace SalesWebMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMVCContext>(options =>
+                   options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
